4c8be0a29b2d40c77705e30631bd47d7e7aad116
pdsong <songpeidong@gmail.com>
2023-03-21 23:01:20 +0800

    3st test3

--- app/server.rb
+++ app/server.rb
@@ -12,9 +12,14 @@ class YourRedisServer
     # Uncomment this block to pass the first stage
     # server = TCPServer.new(@port)
     # client = server.accept
-    server =TCPServer.new(@port)
-    client=server.accept
-    client.write"+PONG\r\n"
+    server = TCPServer.new(@port)
+    client = server.accept
+    # client.write"+PONG\r\n"
+    while !client.eof?
+      x = client.readpartial 1024
+      client.write "+PONG\r\n"
+    end
+    client.close
   end
 end
 
